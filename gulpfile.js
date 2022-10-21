4c7078a5f9a89652c6bb028cdf31200e986cd2ff
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2022-10-21 16:57:08 +0700

    build: add cleaning build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 import gulp from 'gulp';
 import pug from 'gulp-pug';
 import browserSync from 'browser-sync';
+import { deleteAsync } from 'del';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const DIR_NAME = path.dirname(fileURLToPath(import.meta.url));
+
+const BUILD_DIR = path.resolve(DIR_NAME, 'build');
+const SOURCE_DIR = path.resolve(DIR_NAME, 'src')
 
 
 const buildPug = async () => (
-  gulp.src('./src/pug/pages/*.pug')
+  gulp.src(`${SOURCE_DIR}/pug/pages/*.pug`)
     .pipe(pug({}))
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest(BUILD_DIR))
 );
 
 
 const startServer = async () => {
   browserSync.init({
     server: {
-      baseDir: './build',
+      baseDir: BUILD_DIR,
     },
     ui: false,
   });
@@ -21,8 +30,16 @@ const startServer = async () => {
 
 
 const watch = async () => {
-  gulp.watch('./src/pug/**/*.pug').on('change', gulp.series(buildPug, browserSync.reload));
+  gulp.watch(`${SOURCE_DIR}/pug/**/*.pug`).on('change', gulp.series(buildPug, browserSync.reload));
 };
 
 
-export const development = gulp.series(buildPug, startServer, watch);
+const clear = async () => deleteAsync(BUILD_DIR);
+
+
+export const development = gulp.series(
+  clear,
+  buildPug,
+  startServer,
+  watch
+);
